9658a3852693f19442383702fe58bc5eb7a43e0a
samarth-5 <128695366+samarth-5@users.noreply.github.com>
2023-03-27 16:50:59 +0530

    Add files via upload

---
+++ weat.js
@@ -0,0 +1,6 @@
+let place=document.getElementById("location");
+let tv=document.getElementById("tvalue");
+let hv=document.getElementById("hvalue");
+let wv=document.getElementById("wvalue");
+const searchInput=document.getElementById("cit");
+const searchButton=document.getElementById("ico");
